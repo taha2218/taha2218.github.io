de7953235f8ba17dcd3a81b9bb6e6b5befc04e9a
taha2218 <ta.22.ha.18.s@gmail.com>
2019-10-28 04:57:25 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+        window.onload = function() {
+            lax.setup() // init
+
+            const updateLax = () => {
+                lax.update(window.scrollY)
+                window.requestAnimationFrame(updateLax)
+            }
+
+            window.requestAnimationFrame(updateLax)
+        }
+    
